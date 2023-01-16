db25695a0fb23ddc9df3f3d3e66f9cec07dc144c
Mehmet Ali Demir <64283995+mehmetalidemir@users.noreply.github.com>
2023-01-16 19:17:35 +0300

    project added

---
+++ VideoGameApp/VideoGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoGameApp
+//
+//  Created by Mehmet Ali Demir on 16.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
